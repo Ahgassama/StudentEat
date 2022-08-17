e75b89b98458353463beceef85520086541bf835
Ahgassama <ahmad.gassama@gmail.com>
2022-08-17 17:31:22 +0200

    configuration

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 dotenv.config({
-  path: "config/.env",
+  path: "dBconfig/.env",
 });
 require("./models/dbConfig");
 const bodyParser = require("body-parser");
