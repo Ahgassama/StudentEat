ad4abf9a70a4e90053ceaf111a2c194078b5c729
Ahgassama <ahmad.gassama@gmail.com>
2022-08-19 16:58:42 +0200

    middleware CRUD

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,66 +1,34 @@
 const User = require("../models/modelsUser");
 const fs = require("fs");
 exports.getAllUsers = (req, res) => {
-  // Validate request
-  console.log("get All users!! yeaaaa fenneccita");
+  User.find()
+    .then((user) => res.status(200).json(user))
+    .catch((error) => res.status(400).json({ error }));
 };
 exports.getOneUser = (req, res) => {
-  // Validate request
-  console.log("get one user!! yeaaaa fenneccita");
-};
-exports.getAllUsers = (req, res) => {
-  // Validate request
-  console.log("get All users!! yeaaaa fenneccita");
-};
-/*exports.upload = (req, res) => {
-  const userObject = JSON.parse(req.body.user);
-  const user = new User({
-    ...userObject,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${
-      req.file.filename
-    }`,
-  });
-  user
-    .save()
-    .then(() => res.status(201).json({ message: "Objet enregistré!" }))
+  User.findOne({ _id: req.params.id })
+    .then((user) => res.status(200).json(user))
     .catch((error) => res.status(400).json({ error }));
-  // Validate request
-  console.log("get All users!! yeaaaa fenneccita");
-};*/
-/*exports.upload = async (req, res) => {
-  try {
-    const userToUpdate = req.body;
-    const foundUser = await User.findOne({
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!foundUser) {
-      return res.status(404).json({ error: "User non trouvé !" });
-    }
-
-    if (req.file) {
-      userToUpdate.imageUrl = `${req.protocol}://${req.get("host")}/images/${
-        req.file.filename
-      }`;
-      if (foundUser.imageUrl) {
-        fs.unlink(`images/${foundUser.img.split("images/")[1]}`, (err) => {
-          console.error(err);
-        });
+};
+exports.deleteOneUser = (req, res) => {
+  User.findOne({ _id: req.params.id })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send("Utilisateur inexistant");
       }
-    }
+      const filename = user.imageUrl.split("/images/")[1];
+      User.deleteOne({ _id: req.params.id })
+        .then(() => {
+          fs.unlink(`images/${filename}`),
+            () => {
+              res.status(200).json({ message: "Utilisateur supprimé" });
+            };
+        })
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
 
-    for (const key in userToUpdate) {
-      foundUser[key] = userToUpdate[key];
-    }
-    await foundUser.save();
-
-    res.status(200).json({ foundUser });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-};*/
 exports.upload = (req, res) => {
   User.findOne({ _id: req.params.id })
     .then((user) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,11 @@
 const { Router } = require("express");
 const { signup, login } = require("../controllers/authControllers");
-const { upload, getOneUser } = require("../controllers/userControllers");
+const {
+  upload,
+  getOneUser,
+  getAllUsers,
+  deleteOneUser,
+} = require("../controllers/userControllers");
 const authParams = require("../middlewares/authParams");
 
 const router = Router();
@@ -12,16 +17,12 @@ router.post("/login", authParams, login);
 const auth = require("../middlewares/token");
 const multer = require("../middlewares/multer-config");
 
-/*// Route pour recupérer toutes les utilisateurs
-router.get("/", auth, getAllUsers);
-// Route pour recupérer un utilisateur
-Route pour la création d'un utilisateur
-router.post("/", auth, multer, upload);
 // Route pour la modification d'un utilisateur
-router.put("/:id", auth, multer, updateOneUser);
-// Route pour la suppression d'un utilisateur
-router.delete("/:id", auth, deleteOneUser);*/
-//router.post("/", auth, multer);
 router.put("/:id", auth, multer, upload);
+// Route pour recupérer un utilisateur
 router.get("/:id", auth, getOneUser);
+// Route pour recupérer toutes les utilisateurs
+router.get("/", auth, getAllUsers);
+// Route pour la suppression d'un utilisateur
+router.delete("/:id", auth, deleteOneUser);
 module.exports = router;
