98ab3b62a43016bcf48ec5badb0f3c9aeb4c5c2d
changriha neijma <changriha.neijma@orange.fr>
2022-08-20 22:33:46 +0200

    utilisateur supp

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -23,7 +23,7 @@ exports.deleteOneUser = (req, res, next) => {
         });
       }
       User.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+        .then(() => res.status(200).json({ message: "Utilisateur supprimé !" }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
