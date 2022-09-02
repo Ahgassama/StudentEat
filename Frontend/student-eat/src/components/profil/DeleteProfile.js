358692d004198a95e0c0308164a38a48d0832aa4
Ahgassama <ahmad.gassama@gmail.com>
2022-09-02 20:19:27 +0200

    début ajout composant profil

---
+++ Frontend/student-eat/src/components/profil/DeleteProfile.js
@@ -0,0 +1,43 @@
+import React from "react";
+import axios from "axios";
+//fonction de suppression de l'utilisateur
+const DeleteProfile = () => {
+  const handledeleteProfile = (e) => {
+    if (!window.confirm(`Voulez-vous vraiment supprimer votre compte ?`))
+      return;
+
+    const user = JSON.parse(localStorage.getItem("Users"));
+    const id = JSON.parse(localStorage.getItem("Users")).userid;
+    console.log(id);
+    const config = {
+      headers: {
+        Authorization: `bearer ${user.token}`,
+      },
+    };
+
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:3000/api/users/${id}`, config)
+      .then((res) => {
+        if (res.data.errors) {
+          console.log("pas d'utilisateur");
+        } else {
+          alert("Votre compte est bien supprimé");
+          window.location = "/auth";
+          localStorage.clear("Users");
+          console.log(res);
+        }
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+  return (
+    <input
+      type="submit"
+      onClick={handledeleteProfile}
+      value="Désactiver le compte"
+    />
+  );
+};
+export default DeleteProfile;
---
+++ Frontend/student-eat/src/components/profil/Edit.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import DeleteProfile from "./DeleteProfile";
+import UpdatePassword from "./UpdatePassword";
+import "./Edit.scss";
+//Affichage du profil de l'utilisateur
+function DisplayProfile() {
+  const [updateModal, setUpdate] = useState(false);
+  function toggle() {
+    setUpdate(!updateModal);
+  }
+  const handleModals = (e) => {
+    if (e.target.id === "password") {
+      setUpdate(true);
+    }
+  };
+  const [data, setData] = useState([]);
+  const user = JSON.parse(localStorage.getItem("Users"));
+  console.log(JSON.parse(localStorage.getItem("Users")).userid);
+  const config = {
+    headers: {
+      Authorization: `bearer ${user.token}`,
+    },
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        `http://localhost:3000/api/users/${
+          JSON.parse(localStorage.getItem("Users")).userid
+        }`,
+        config
+      );
+      console.log(result.data);
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="form-profil">
+      <div className="profil-container">
+        <form action="" id="profil-form">
+          <label htmlFor="name">Nom</label>
+          <br />
+          <input type="text" name="name" id="name" placeholder={data.name} />
+
+          <br />
+          <label htmlFor="surname">Prénom</label>
+          <br />
+          <input
+            type="text"
+            name="surname"
+            id="surname"
+            placeholder={data.surname}
+          />
+
+          <br />
+          <label htmlFor="email">E-mail</label>
+          <br />
+          <input type="text" name="email" id="email" placeholder={data.email} />
+
+          <br />
+          <DeleteProfile />
+
+          <br />
+          <input
+            type="button"
+            name="password"
+            id="password"
+            value="Modifier mon mot de passe"
+            onClick={handleModals && toggle}
+          />
+        </form>
+        {updateModal && <UpdatePassword />}
+      </div>
+    </div>
+  );
+}
+export default DisplayProfile;
