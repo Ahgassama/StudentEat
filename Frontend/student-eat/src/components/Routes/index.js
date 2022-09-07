c7b22b27f870e167f4512ff3bbb0ef3b2d2afd6a
Ahgassama <ahmad.gassama@gmail.com>
2022-09-07 16:59:38 +0200

    route ok appel api ko modif à faire

--- Frontend/student-eat/src/components/Routes/index.js
+++ Frontend/student-eat/src/components/Routes/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Connexion from "../../pages/Connexion";
+import Home from "../../pages/Home";
 const index = () => {
   return (
     <div>
       <Routes>
         <Route default path="/auth" element={<Connexion />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
