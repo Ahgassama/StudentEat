b131296cbd2c3ace46c51ca7ec59c7bf0aede281
Ahgassama <ahmad.gassama@gmail.com>
2022-09-13 12:59:29 +0200

    début template footer

--- Frontend/student-eat/src/components/footer/footer.js
+++ Frontend/student-eat/src/components/footer/footer.js
@@ -1,1 +1,12 @@
 import React from "react";
+import "./footer.scss";
+const Footer = () => {
+  return (
+    <footer className="footer-home">
+      <div className="logo">
+        <img src="./images/logo.jpg" alt="img-logo" />
+      </div>
+    </footer>
+  );
+};
+export default Footer;
--- Frontend/student-eat/src/pages/home.js
+++ Frontend/student-eat/src/pages/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
 
 const Home = () => {
   return (
